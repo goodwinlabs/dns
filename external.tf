4b712c3649ec6158b38ef31184931461e9555dac
Ryan Goodwin <goodwinryan2@gmail.com>
2026-02-23 19:19:18 -0500

    Mark public ip as sensitive value

--- external.tf
+++ external.tf
@@ -6,10 +6,14 @@ data "cloudflare_zone" "ryanjgoodwin_com" {
   zone_id = "9945ffa50f42da81c5aafebbeadef051"
 }
 
+locals {
+  services = yamldecode(file("${path.module}/external-dns.yaml"))
+}
+
 resource "cloudflare_dns_record" "base" {
   zone_id = data.cloudflare_zone.ryanjgoodwin_com.id
   name    = "home"
-  content   = data.http.public_ip.response_body
+  content = sensitive(data.http.public_ip.response_body)
   type    = "A"
   ttl     = 1
   proxied = true
@@ -18,7 +22,7 @@ resource "cloudflare_dns_record" "base" {
 resource "cloudflare_dns_record" "root" {
   zone_id = data.cloudflare_zone.ryanjgoodwin_com.id
   name    = "@"
-  content   = "192.1.2.1"
+  content = "192.1.2.1"
   type    = "A"
   ttl     = 1
   proxied = true
@@ -27,31 +31,28 @@ resource "cloudflare_dns_record" "root" {
 resource "cloudflare_dns_record" "www" {
   zone_id = data.cloudflare_zone.ryanjgoodwin_com.id
   name    = "www"
-  content   = data.cloudflare_zone.ryanjgoodwin_com.name
+  content = data.cloudflare_zone.ryanjgoodwin_com.name
   type    = "CNAME"
   ttl     = 1
   proxied = true
 }
 
-locals {
-  services = yamldecode(file("${path.module}/external-dns.yaml"))
-}
-
 module "proxy_service" {
-  source = "./modules/proxy_service"
+  source   = "./modules/proxy_service"
   for_each = local.services
 
-  name                  = each.value.name
-  zone_id               = data.cloudflare_zone.ryanjgoodwin_com.id
-  base_record           = "${cloudflare_dns_record.base.name}"
-  # ip                    = data.http.public_ip.response_body
-  forward_host          = each.value.forward_host
-  forward_port          = each.value.forward_port
-  forward_scheme        = lookup(each.value, "forward_scheme", "http")
-  letsencrypt_email     = var.letsencrypt_email
-  proxied               = lookup(each.value, "proxied", true)
-  caching_enabled       = lookup(each.value, "caching_enabled", true)
+  name        = each.value.name
+  zone_id     = data.cloudflare_zone.ryanjgoodwin_com.id
+  base_record = cloudflare_dns_record.base.name
+  # ip          = sensitive(data.http.public_ip.response_body)
+
+  forward_host            = each.value.forward_host
+  forward_port            = each.value.forward_port
+  forward_scheme          = lookup(each.value, "forward_scheme", "http")
+  letsencrypt_email       = var.letsencrypt_email
+  proxied                 = lookup(each.value, "proxied", true)
+  caching_enabled         = lookup(each.value, "caching_enabled", true)
   allow_websocket_upgrade = lookup(each.value, "allow_websocket_upgrade", false)
-  advanced_config       = lookup(each.value, "advanced_config", "")
+  advanced_config         = lookup(each.value, "advanced_config", "")
 }
 
